67bf1d204bd6cdaeaa7191c75da04cdb3e54dc22
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-18 11:46:36 +0200

    Added command handler

---
+++ org/nulldev/pluginjs/ScriptCommandExecutor.java
@@ -0,0 +1,25 @@
+package org.nulldev.pluginjs;
+
+import java.util.logging.Level;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+class ScriptCommandExecutor implements CommandExecutor {
+    private final EasyScript plugin;
+    private final CommandCallback callback;
+
+    public ScriptCommandExecutor(EasyScript plugin, CommandCallback callback) {
+        this.plugin = plugin;
+        this.callback = callback;
+    }
+
+    public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+        try { return callback.callback(cs, string, strings); } 
+        catch (RuntimeException ex) {
+            plugin.getLogger().log(Level.WARNING, ex.getMessage());
+            return false;
+        }
+    }
+    
+}
