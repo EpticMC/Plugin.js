c3de00fe7841d9ff50de00c352721b86d805802c
NullDev (Shadow) <NL-Dev@yandex.com>
2017-09-18 11:44:22 +0200

    Added callback handler

---
+++ org/nulldev/pluginjs/CommandCallback.java
@@ -0,0 +1,5 @@
+package org.nulldev.pluginjs;
+
+import org.bukkit.command.CommandSender;
+
+public interface CommandCallback { public boolean callback(CommandSender sender, String command, String[] args); }
